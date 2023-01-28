8918ac228add1e869a82e0b070b37262e50ae3bc
Joaquin Alfaro <joaquin.alfaro@gmail.com>
2023-01-28 15:48:38 +0100

    Create magnolia module to train AI system.

---
+++ ai-training/src/main/java/org/formentor/AiTrainingModule.java
@@ -0,0 +1,13 @@
+package org.formentor;
+
+/**
+ * This class is optional and represents the configuration for the ai-training module.
+ * By exposing simple getter/setter/adder methods, this bean can be configured via content2bean
+ * using the properties and node from <tt>config:/modules/ai-training</tt>.
+ * If you don't need this, simply remove the reference to this class in the module descriptor xml.
+ * See https://documentation.magnolia-cms.com/display/DOCS/Module+configuration for information about module configuration.
+ */
+public class AiTrainingModule {
+    /* you can optionally implement info.magnolia.module.ModuleLifecycle */
+
+}
